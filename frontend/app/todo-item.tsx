b0e7b7f7aea7706c56d48aee527125317757a8bd
florian.serger <florian.serger@arag.de>
2025-12-05 14:15:44 +0100

    implement frontend

---
+++ frontend/app/todo-item.tsx
@@ -0,0 +1,24 @@
+import { TodoItem } from "./types";
+
+type TodoItemProps = {
+    item: TodoItem;
+    deleteItem: (id: number) => Promise<void>;
+    toggleItem: (id: number) => Promise<void>;
+}
+
+export default function Item({ item, deleteItem, toggleItem }: TodoItemProps) {
+    const { id, name, completed } = item;
+    return (
+        <div key={item.id} className={`item ${completed ? "item-completed" : ""}`}>
+            <span className="name">{name}</span>
+            <div className="flex gap-2">
+                <span className="icon material-symbols-outlined cursor-pointer" onClick={() => toggleItem(id)}>
+                    {completed ? "check_box" : "check_box_outline_blank"}
+                </span>
+                <span className="material-symbols-outlined cursor-pointer text-red-500" onClick={() => deleteItem(id)}>
+                     delete
+                </span>
+            </div>
+        </div>
+    )
+}
